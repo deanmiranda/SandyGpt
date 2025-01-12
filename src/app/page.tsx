bb4a57ffdf12718216e97fe1322dbd0adfcd8a49
deanmiranda <dean.m.miranda@gmail.com>
2025-01-12 13:50:32 -0500

    SandyGPT - Initializing open ai with auth key and ui components.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
-export default function Home() {
+import { auth as getServerSession } from "@/auth";
+
+import { Separator } from "@/components/ui/separator";
+import Chat from "@/app/components/Chat";
+
+export default async function Home() {
+  const session = await getServerSession();
+  console.log('session is ', session);
+
   return (
-    <main className="p5">
-      <h1 className="p-5 text-4xl font-bold text-center">Welcome to Sandy GPT Chat</h1>
+    <main className="p-5">
+      <h1 className="text-4xl font-bold">Welcome To GPT Chat</h1>
+      {!session?.user && <div>You need to log in to use this chat.</div>}
+      {session?.user && (
+        <>
+          <Separator className="my-5" />
+          <Chat />
+        </>
+      )}
     </main>
   );
 }
