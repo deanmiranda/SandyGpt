4bdc7be72732e1e8d1000543d32407f75c4661ba
deanmiranda <dean.m.miranda@gmail.com>
2025-01-15 18:39:41 -0500

    SandyGpt - Corrections for production server for schema of email -> username

--- src/auth.ts
+++ src/auth.ts
@@ -6,7 +6,7 @@ declare module "next-auth" {
   interface Session {
     user: {
       name?: string | null
-      email?: string | null
+      // email?: string | null
       image?: string | null
       username?: string | null
     }
